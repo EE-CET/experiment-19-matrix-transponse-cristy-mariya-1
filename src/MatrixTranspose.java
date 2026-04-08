18f2a23bb35840f22f094a1d6a86bbd8280c7b44
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-08 11:46:45 +0530

    Update MatrixTranspose.java

--- src/MatrixTranspose.java
+++ src/MatrixTranspose.java
@@ -1,8 +1,39 @@
+import java.util.Scanner;
+
 public class MatrixTranspose {
-   
-        // TODO: Read N
-        // TODO: Read the N x N matrix
-        // TODO: Transpose the matrix INPLACE (Swap mat[i][j] with mat[j][i])
-        // TODO: Print the modified matrix
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read size
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+        // Input matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // In-place transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // Swap matrix[i][j] and matrix[j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Print transposed matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
 }
