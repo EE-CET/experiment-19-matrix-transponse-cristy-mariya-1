2e96a27905c41367baca5b5ce3b92f238d18748a
cristy-mariya <151554556+cristy-mariya@users.noreply.github.com>
2026-04-08 11:48:42 +0530

    Update MatrixTranspose.java

--- src/MatrixTranspose.java
+++ src/MatrixTranspose.java
@@ -4,12 +4,10 @@ public class MatrixTranspose {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Read size
         int n = sc.nextInt();
-
         int[][] matrix = new int[n][n];
 
-        // Input matrix
+        // Input
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = sc.nextInt();
@@ -19,17 +17,19 @@ public class MatrixTranspose {
         // In-place transpose
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                // Swap matrix[i][j] and matrix[j][i]
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
 
-        // Print transposed matrix
+        // Print without trailing space
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(matrix[i][j]);
+                if (j != n - 1) {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
